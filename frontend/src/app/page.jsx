0c4e437583d3f9f960535293a2adc65aa92efdec
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-07-15 20:09:00 +0700

    setup

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Cards } from "@/components/cards";
+
+const Home = () => {
+  return (
+    <>
+      <h1>code must best practice and clean code</h1>
+      <h2>mudah dibaca developer lain.</h2>
+      <Cards.CardAchievement />
+    </>
+  );
+};
+
+export default Home;
---
+++ frontend/src/components/cards/index.jsx
@@ -0,0 +1,10 @@
+const Cards = {
+  CardAchievement: () => {
+    return <div>card achievement</div>;
+  },
+  CardRecent: () => {
+    return <div>card recent</div>;
+  },
+};
+
+export { Cards };
