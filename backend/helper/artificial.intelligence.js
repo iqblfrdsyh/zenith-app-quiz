48ed6f77050ac74c52aeeeffce04dbc60f2c542b
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-08-15 17:00:36 +0700

    add ai

---
+++ backend/helper/artificial.intelligence.js
@@ -0,0 +1,34 @@
+const { HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
+
+const GoogleGenerativeAI = require("@google/generative-ai").GoogleGenerativeAI;
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  safetySettings: [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+  ],
+});
+
+exports.GenAI = async (req, res) => {
+  try {
+    const { message, history } = req.body;
+
+    const chat = model.startChat({
+      history,
+    });
+
+    let result = await chat.sendMessage(message);
+    const response = await result.response;
+    const text = response.text();
+
+    res.status(200).json({ status: 200, response: text });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: 500, error: "Failed to interact with the AI model" });
+  }
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,6 @@
 const userRoute = require("./user");
 const leaderboardRoute = require("./leaderboard");
 const achievementRoute = require("./achievement");
+const AIRoute = require("./AI.route");
 
-module.exports = { userRoute, leaderboardRoute, achievementRoute };
+module.exports = { userRoute, leaderboardRoute, achievementRoute, AIRoute };
