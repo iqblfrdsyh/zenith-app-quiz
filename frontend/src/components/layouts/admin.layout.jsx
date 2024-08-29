0441d45279f1e2ef7a8d35331fb6469b13372c4c
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-08-30 00:03:24 +0700

    Change code in admin.layout.js

--- frontend/src/components/layouts/admin.layout.jsx
+++ frontend/src/components/layouts/admin.layout.jsx
@@ -2,29 +2,42 @@ import React from "react";
 import SidebarAdmin from "../sidebar";
 import { usePathname } from "next/navigation";
 
+const menuItems = [
+  { href: "/admin/dashboard", label: "Dashboard" },
+  { href: "/admin/quiz", label: "Quiz" },
+  { href: "/admin/category", label: "Category" },
+  { href: "/admin/topic", label: "Topic" },
+  { href: "/admin/leaderboard", label: "Leaderboard" },
+  { href: "/admin/users", label: "Users" },
+];
+
+const getActiveTitle = (pathname, menuItems) => {
+  const currentPath = pathname.split("/admin/")[1] || "";
+  return (
+    menuItems.find((item) => item.href.includes(currentPath))?.label ||
+    "Dashboard"
+  );
+};
+
 export function AdminLayout({ children }) {
   const pathname = usePathname();
-  const currentPath = pathname.split("/admin/")[1] || "";
-
-  const menuItems = [
-    { href: "/admin/dashboard", label: "Dashboard" },
-    { href: "/admin/quiz", label: "Quiz" },
-    { href: "/admin/category", label: "Category" },
-    { href: "/admin/leaderboard", label: "Leaderboard" },
-    { href: "/admin/users", label: "Users" },
-  ];
-
-  const activeTitle =
-    menuItems.find((item) => item.href.includes(currentPath))?.label ||
-    "Dashboard";
+  const isHiddenSidebar = ["/admin/signin"].includes(pathname);
+  const activeTitle = getActiveTitle(pathname, menuItems);
 
   return (
     <div className="flex">
-      <SidebarAdmin menuItems={menuItems} />
-      <main className="absolute top-10 p-5 overflow-x-hidden md:left-64 md:top-0">
-        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold p-5 hidden md:block">
-          {activeTitle}
-        </h1>
+      {!isHiddenSidebar && <SidebarAdmin menuItems={menuItems} />}
+
+      <main
+        className={`absolute top-10 p-5 overflow-x-hidden md:top-0 ${
+          isHiddenSidebar ? "" : "md:ml-64"
+        }`}
+      >
+        {!isHiddenSidebar && (
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold p-5 hidden md:block">
+            {activeTitle}
+          </h1>
+        )}
         {children}
       </main>
     </div>
